f11eb818e8755f83b98c087161f54429ee798d8c
Vladyslav Moisieienkov <vladyslav.moisieienkov@gmail.com>
2018-09-11 18:02:01 +0200

    Add experimental infinitive loop for test purpose

--- dask-k8s.py
+++ dask-k8s.py
@@ -1,4 +1,9 @@
 from dask_kubernetes import KubeCluster
+import time
 
-cluster = KubeCluster.from_yaml('worker-spec.yml')
+cluster = KubeCluster.from_yaml('worker-spec.yaml')
 cluster.adapt(minimum=1, maximum=3)
+print("[!] Start loop...")
+while True:
+    time.sleep(1)
+    print("tick")
