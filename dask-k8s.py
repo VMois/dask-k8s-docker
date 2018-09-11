1aee480a559ad70ff248c0e8b5b57338e9cfd5ad
Vladyslav Moisieienkov <vladyslav.moisieienkov@gmail.com>
2018-09-11 22:28:19 +0200

    Add port and diagnostic_port numbers to main python script

--- dask-k8s.py
+++ dask-k8s.py
@@ -1,7 +1,10 @@
 from dask_kubernetes import KubeCluster
 import time
 
-cluster = KubeCluster.from_yaml('worker-spec.yaml')
+cluster = KubeCluster.from_yaml(
+    'worker-spec.yaml', 
+    port=8786,
+    diagnostics_port=8787)
 cluster.adapt(minimum=1, maximum=3)
 print("[!] Start loop...")
 while True:
