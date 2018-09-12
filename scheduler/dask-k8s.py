df793ab31c060f5f4715a54b705267abfb5ff72a
Vladyslav Moisieienkov <vladyslav.moisieienkov@gmail.com>
2018-09-12 13:07:16 +0200

    Seperate in two Docker files: for scheduler and for worker

---
+++ scheduler/dask-k8s.py
@@ -0,0 +1,10 @@
+from dask_kubernetes import KubeCluster
+import time
+
+cluster = KubeCluster.from_yaml(
+    'worker-spec.yaml', 
+    port=8786,
+    diagnostics_port=8787)
+cluster.adapt(minimum=1, maximum=3)
+while True:
+    time.sleep(1)
