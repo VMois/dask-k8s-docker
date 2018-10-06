99f1701b70bd9cb47e55f14c0ccb30846773ffb1
Vladyslav Moisieienkov <vladyslav.moisieienkov@gmail.com>
2018-10-06 18:20:02 +0200

    Save progress

--- scheduler/dask-k8s.py
+++ scheduler/dask-k8s.py
@@ -1,10 +1,24 @@
 from dask_kubernetes import KubeCluster
+import os
 import time
 
 cluster = KubeCluster.from_yaml(
     'worker-spec.yaml', 
     port=8786,
     diagnostics_port=8787)
-cluster.adapt(minimum=1, maximum=8)
+
+# Get ENV variables for cluster scaling config
+# More info: https://github.com/dask/distributed/blob/master/distributed/deploy/adaptive.py
+min_workers_number = os.getenv('DASK_CLUSTER_MIN_WORKERS', 1)
+max_workers_number = os.getenv('DASK_CLUSTER_MAX_WORKERS', 8)
+startup_cost = os.getenv('DASK_CLUSTER_STARTUP_COST', '10s')
+target_duration = os.getenv('DASK_CLUSTER_TARGET_DURATION', '10s')
+
+cluster.adapt(
+    minimum=min_workers_number,
+    maximum=max_workers_number,
+    startup_cost=startup_cost,
+    target_duration=target_duration
+)
 while True:
-    time.sleep(1)
+    time.sleep(0.1)
